562fa3e3650a99b2a830ab84c0d4a53245ce6f6b
Ikkino <50905453+Guillaume-Lebegue@users.noreply.github.com>
2023-10-16 23:01:03 +0200

    👷 : Adding CI (#1)

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,5 +2,13 @@ module.exports = {
   transform: {'^.+\\.ts?$': 'ts-jest'},
   testEnvironment: 'node',
   testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80
+    }
+  }
 };
